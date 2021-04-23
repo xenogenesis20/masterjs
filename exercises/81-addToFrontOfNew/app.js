6c7e45549621dd916556037f36cfbf3ce6713114
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-23 17:47:32 +0000

    sigh

--- exercises/81-addToFrontOfNew/app.js
+++ exercises/81-addToFrontOfNew/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+let input = [1, 2];
+
+// var output = addToFrontOfNew(input, 3)
+
+const addToFrontOfNew = (input,num) =>{
+    
+       let output = input.map((x) =>{ input.unshift(num) })
+    
+    return output
+}
+
+
+console.log(addToFrontOfNew(input,3));
+
+
