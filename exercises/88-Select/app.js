6fc4d5dd3c0e43e574d81c85e648c26192942dad
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-25 21:12:50 +0000

    progress

--- exercises/88-Select/app.js
+++ exercises/88-Select/app.js
@@ -1,1 +1,19 @@
 // Write your function here
+
+const select = (array,object) => {
+    let newObj = {};
+
+    
+
+}
+
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+var output = select(arr, obj);
+console.log(output); // --> { a: 1, c: 3 }
+
--- exercises/89-getElements/app.js
+++ exercises/89-getElements/app.js
@@ -1,1 +1,22 @@
 // Write your function here
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+
+const getElementsThatEqual10AtProperty = (object, prop) =>{
+    if (object[prop].lenght == 0) return []
+    if (object[prop].forEach(element => { element !== 10})) return []
+    if (!Array.isArray(object[prop])) return []
+    
+
+      let newArray = object[prop].filter(num => num == 10).map(item => item )
+   
+    return newArray
+}
+
+
+
+
+
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
